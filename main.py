d74e53ad38db583778021253e808ae265c4db0d9
ali mohammad <alimohammad0816@gmail.com>
2020-12-04 19:23:12 +0330

    account func is completed

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import requests
 
-TOKEN = '1330763196:AAEPyj60bkRiYCkEDHdL1LXCMCTkiwibh08'
-telegram_api = f"https://api.telegram.org/bot{TOKEN}/"
+TOKEN = '1330763196:AAH3HHUfzfU4MSufuSWnbEZIQiQ--jy0Oug'
+url = f"https://api.telegram.org/bot{TOKEN}/"
 
 # users data
 users = {
@@ -31,20 +31,32 @@ user_state = {
 }
 
 
-# ثبتنام ، آپدیت اکانت ، دلیت اکانت
+# get request for new messages
+def get_update(url):
+    old_req = requests.get(f"{url}getUpdates").json()
+    old_req = old_req["result"]
+    offset = old_req[-1]['update_id']
+    while True:
+        req = requests.get(f"{url}getUpdates?offset={offset}").json()
+        req = req["result"][-1]
+        if not offset == req['update_id']:
+            return req
+
+
+# Signup, Update, Delete Account with Confirm.
 def account(req, url):
-    message = req["message"]["text"]
-    # user info
+    global user_state
+    global users
+    client_message = req["message"]["text"]
     username = req['message']["from"]['username']
-    username = f"@{username}"
+    username = f"{username}"
     user_id = req['message']["from"]['id']
     first_name = req['message']["from"]['first_name']
     try:
         last_name = req['message']["from"]['last_name']
     except KeyError:
         last_name = "نامشخص"
-    if message.lower() == "/signup":
-        global users
+    if client_message.lower() == "/signup":
         if username not in users:
             simple = {
                 username: {
@@ -56,7 +68,12 @@ def account(req, url):
                 }
             }
             users.update(simple)
-    elif message.lower() == '/update':
+            text = "ثبتنام شما با موفقیت انجام شد."
+            requests.post(f"{url}sendMessage?chat_id={user_id}&text={text}")
+        else:
+            text = "شما قبلا ثبتنام کرده اید."
+            requests.post(f"{url}sendMessage?chat_id={user_id}&text={text}")
+    elif client_message.lower() == '/updateaccount':
         if username in users:
             simple = {
                 username: {
@@ -68,169 +85,111 @@ def account(req, url):
                 }
             }
             users.update(simple)
-    elif message.lower() == '/delete':
+            text = "پروفایل شما با موفقیت آپدیت شد."
+            requests.post(f"{url}sendMessage?chat_id={user_id}&text={text}")
+        else:
+            text = "شما قبلا ثبتنام نکرده اید."
+            requests.post(f"{url}sendMessage?chat_id={user_id}&text={text}")
+    elif client_message.lower() == '/deleteaccount':
         if username in users:
-            users.pop(username)
+            user_state[username] = "waitforconfirm"
+            text = "آیا از حذف پروفایل خود اطمینان دارید؟برای تایید بله ،و در صورت انصراف خیر را ارسال کنید."
+            requests.post(f"{url}sendMessage?chat_id={user_id}&text={text}")
+    elif username in users:
+        if username in user_state:
+            if user_state[username] == 'waitforconfirm':
+                if client_message == 'بله':
+                    users.pop(username)
+                    text = "پروفایل شما با موفقیت حذف شد."
+                    requests.post(f"{url}sendMessage?chat_id={user_id}&text={text}")
+                elif client_message == 'خیر':
+                    user_state[username] = ''
+                    text = "شما از حذف پروفایل خود انصراف دادید."
+                    requests.post(f"{url}sendMessage?chat_id={user_id}&text={text}")
+                else:
+                    text = "دستور وارد شده صحیح نمیباشد، لطفا دوباره امتحان کنید."
+                    requests.post(f"{url}sendMessage?chat_id={user_id}&text={text}")
 
 
 # نمایش محصولات ، افزودن محصول به سبدخرید ، نمایش و مدیریت سبد خرید
-def product_managing(request):
-    pass
+def product_managing(req, url):
+    global user_state
+    global users
+    client_message = req["message"]["text"]
+    username = f"{req['message']['from']['username']}"
+    user_id = req['message']["from"]['id']
+    if username in users:
+        if client_message.lower() == '/products':
+            text = "محصولات ما : \n"
+            for i in products:
+                text += f"{products[i]['name']}, \n"
+            requests.post(f"{url}sendMessage?chat_id={user_id}&text={text}")
+        elif client_message.lower() == '/mycard':
+            user_card = users[username]['card']
+            if user_card == []:
+                text = "سبد خرید شما خالی است."
+                requests.post(f"{url}sendMessage?chat_id={user_id}&text={text}")
+            if not user_card == []:
+                text = "سبد خرید شما : \n"
+                for i in user_card:
+                    text += f"{i['name']}, \n"
+                requests.post(f"{url}sendMessage?chat_id={user_id}&text={text}")
+        elif client_message.lower() == '/additem':
+            user_state[username] = 'additem'
+            text = 'شما در بخش خرید هستید ، برای افزودن محصولات آیدی محصول مورد نظر را وارد کنید.'
+            requests.post(f"{url}sendMessage?chat_id={user_id}&text={text}")
+        elif username in user_state:
+            print(user_state[username])
+            if user_state[username] == 'additem':
+                if client_message == 'خروج':
+                    text = "به بخش محصولات برگشتید."
+                    requests.post(f"{url}sendMessage?chat_id={user_id}&text={text}")
+                    user_state[username] = 'product_mode'
+                    print(user_state[username])
+                else:
+                    item = products[client_message]
+                    users[username]['card'].append(item)
+                    text = f"محصول {item['name']} اضافه شد.\n"
+                    text += "برای افزودن محصول جدید ،آیدی آن را وارد کنید.\n"
+                    text += "برای خروج از این بخش ، کلمه خروج را ارسال کنید."
+                    requests.post(f"{url}sendMessage?chat_id={user_id}&text={text}")
+    else:
+        text = "برای استفاده از امکانات این برنامه ابتدا ثبتنام کنید. برای ثبتنام از دستور /signup استفاده کنید."
+        requests.post(f"{url}sendMessage?chat_id={user_id}&text={text}")
 
 
 # ثبت سفارش و پرداخت نهایی
-def payment(request):
+def payment(req, url):
     pass
 
 
-# get request for new messages
-def get_update(url):
-    pass
-
-
-def telegram_bot_future():
-    new_req = new_req = requests.get(f"{telegram_api}getUpdates").json()
-    new_req = new_req["result"]
-    old_len = len(new_req)
-    while True:
-        # get new update every second
-        new_req = requests.get(f"{telegram_api}getUpdates").json()
-        new_req = new_req["result"]
-        # if we got new message
-        if old_len < len(new_req):
-            old_len = len(new_req)
-            new_req = new_req[-1]
-            new_req_text = new_req["message"]["text"]
-            # user info
-            username = new_req['message']["from"]['username']
-            username = f"@{username}"
-            user_id = new_req['message']["from"]['id']
-            first_name = new_req['message']["from"]['first_name']
-            try:
-                last_name = new_req['message']["from"]['last_name']
-            except KeyError:
-                last_name = "نامشخص"
-
-            if new_req_text == "/start":
-                text = '''
-                سلام به ربات ما خوش آمدید،
-                برای استفاده از امکانات ربات ابتدا ثبتنام کنید.
-                برای ثبتنام از دستور /signUp استفاده کنید
-                '''
-                requests.post(f"{telegram_api}sendMessage?chat_id={user_id}&text={text}")
-            elif new_req_text.lower() == '/signup':
-                if username not in users:
-                    users.update({
-                        username: {
-                            "username": username,
-                            'id': user_id,
-                            'first_name': first_name,
-                            'last_name': last_name,
-                            'card': []}})
-                    text = f"ثبتنام شما انجام شد، نام شما {first_name} ،نام خانوادگی شم {last_name} ،نام کاربری شما " \
-                           f"{username}"
-                    requests.post(f"{telegram_api}sendMessage?chat_id={user_id}&text={text}")
-                else:
-                    text = "شما قبلا در این ربات ثبتنام کرده اید..."
-                    requests.post(f"{telegram_api}sendMessage?chat_id={user_id}&text={text}")
-            elif new_req_text.lower() == '/updateaccount':
-                if username in users:
-                    users.update({
-                        username: {
-                            "username": username,
-                            'user_id': user_id,
-                            'first_name': first_name,
-                            'last_name': last_name,
-                        }})
-                    text = f"بروزرسانی پروفایل شما انجام شد."
-                    requests.post(f"{telegram_api}sendMessage?chat_id={user_id}&text={text}")
-                else:
-                    text = "شما در گذشته ثبتنام نکرده اید."
-            elif new_req_text.lower() == "/deleteaccount":
-                if username in users:
-                    text = "حساب کاربری شما حذف خواهد شد، ادامه می دهید؟برای متوقف کردن عملیات " \
-                           "/exit و برای ادامه  /yesdeleteaccount را وارد کنید."
-                    requests.post(f"{telegram_api}sendMessage?chat_id={user_id}&text={text}")
-                else:
-                    text = "شما در گذشته ثبتنام نکرده اید."
-                    requests.post(f"{telegram_api}sendMessage?chat_id={user_id}&text={text}")
-            elif new_req_text.lower() == "/yesdeleteaccount":
-                if username in users:
-                    users.pop(username)
-                    text = 'حساب شما با موفقیت پاک شد.'
-                    requests.post(f"{telegram_api}sendMessage?chat_id={user_id}&text={text}")
-            elif new_req_text.lower() == "/products":
-                text = 'محصولات ما :'
-                requests.post(f"{telegram_api}sendMessage?chat_id={user_id}&text={text}")
-                for i in products:
-                    product_id = i
-                    product = products[i]['name']
-                    text = f"نام محصول {product} آیدی محصول {product_id}"
-                    requests.post(f"{telegram_api}sendMessage?chat_id={user_id}&text={text}")
-
-                text = 'برای افزودن محصول به سبد خرید /additem محصول را وارد کنید.'
-            elif new_req_text == '/additem':
-                text = 'برای افزودن محصول به سبد خرید /id محصول را وارد کنید.'
-                requests.post(f"{telegram_api}sendMessage?chat_id={user_id}&text={text}")
-            elif new_req_text == '/1':
-                if username in users:
-                    users[username]['card'].append(products['1'])
-                    text = f"{products['1']['name']}اضافه شد"
-                    requests.post(f"{telegram_api}sendMessage?chat_id={user_id}&text={text}")
-            elif new_req_text == '/2':
-                if username in users:
-                    users[username]['card'].append(products['2'])
-                    text = f"{products['2']['name']}اضافه شد"
-                    requests.post(f"{telegram_api}sendMessage?chat_id={user_id}&text={text}")
-            elif new_req_text == '/3':
-                if username in users:
-                    users[username]['card'].append(products['3'])
-                    text = f"{products['3']['name']}اضافه شد"
-                    requests.post(f"{telegram_api}sendMessage?chat_id={user_id}&text={text}")
-            elif new_req_text == '/4':
-                if username in users:
-                    users[username]['card'].append(products['4'])
-                    text = f"{products['4']['name']}اضافه شد"
-                    requests.post(f"{telegram_api}sendMessage?chat_id={user_id}&text={text}")
-            elif new_req_text == '/5':
-                if username in users:
-                    print(users[username])
-                    users[username]['card'].append(products['5'])
-                    text = f"{products['5']['name']}اضافه شد"
-                    requests.post(f"{telegram_api}sendMessage?chat_id={user_id}&text={text}")
-            elif new_req_text == '/6':
-                if username in users:
-                    users[username]['card'].append(products['6'])
-                    text = f"{products['6']['name']}اضافه شد"
-                    requests.post(f"{telegram_api}sendMessage?chat_id={user_id}&text={text}")
-            elif new_req_text == '/7':
-                if username in users:
-                    users[username]['card'].append(products['7'])
-                    text = f"{products['7']['name']}اضافه شد"
-                    requests.post(f"{telegram_api}sendMessage?chat_id={user_id}&text={text}")
-            elif new_req_text == '/mycard':
-                if username in users:
-                    text = 'محصولات موجود در سبد خرید شما'
-                    requests.post(f"{telegram_api}sendMessage?chat_id={user_id}&text={text}")
-                    for i in users[username]['card']:
-                        text = f"{i['name']}"
-                        requests.post(f"{telegram_api}sendMessage?chat_id={user_id}&text={text}")
-            elif new_req_text == '/del_1':
-                if username in users:
-                    # print(users[username]["card"])
-                    product_id = new_req_text.split('_')
-                    product_name = products[product_id[1]]['name']
-                    for i in users[username]["card"]:
-                        # print(i)
-                        if product_name == i['name']:
-                            text = f'{i["name"]}با موفقیت حذف شد'
-                            requests.post(f"{telegram_api}sendMessage?chat_id={user_id}&text={text}")
-                            break
-            else:
-                user_id = new_req['message']["from"]['id']
-                text = f"دوست عزیز دستورات شما قابل فهم برای ربات نیست، لطفا از دستورات پیش فرض ربات استفاده کنید."
-                requests.post(f"{telegram_api}sendMessage?chat_id={user_id}&text={text}")
+def executing():
+    global url
+    global user_state
+    req = get_update(url)
+    username = req['message']["from"]['username']
+    user_id = req['message']['from']['id']
+    client_message = req["message"]["text"]
+    if client_message.lower() == '/product':
+        text = "شما وارد بخش محصولات شدید.برای خارج شدن ، کلمه خروج را وارد کنید."
+        requests.post(f"{url}sendMessage?chat_id={user_id}&text={text}")
+        user_state[username] = 'product_mode'
+    if username in user_state:
+        if user_state[username] == 'product_mode':
+            if client_message.lower() == 'خروج':
+                text = "شما به صفحه اصلی بازگشتید."
+                requests.post(f"{url}sendMessage?chat_id={user_id}&text={text}")
+                user_state[username] = ''
+    account(req=req, url=url)
+    if username in user_state:
+        if user_state[username] == 'product_mode':
+            product_managing(req=req, url=url)
 
 
 if __name__ == "__main__":
-    telegram_bot_future()
+    print("app is Running now...")
+    while True:
+        print(user_state)
+        executing()
+
+
